05e4a770f42e992c974efbd17a60eeab210f3723
Dell <Dell@10.0.0.3>
2017-03-11 22:05:10 +0200

    first commit

---
+++ TestGit/src/test/java/test2/TestGit/SimpleTest.java
@@ -0,0 +1,16 @@
+package test2.TestGit;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleTest {
+
+	@Test
+	public void simpleTestPass()
+	{
+		int a=1;
+		int b=1;
+		assertTrue(a+b==2);
+	
+	}
+}
---
+++ TestGit/src/test/java/test2/TestGit/test2.java
@@ -0,0 +1,10 @@
+package test2.TestGit;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
